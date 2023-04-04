4bafbe025468cd532bed9ff6cf44abdf6b92b795
Neel <ezebarazarte@gmail.com>
2023-04-03 20:13:37 -0400

    mensaje de error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import WeatherCard from './components/WeatherCard';
 import Loading from './components/Loading';
 import Select from 'react-select'
 import VideoComponent from './components/VideoComponent';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationPinLock } from '@fortawesome/free-solid-svg-icons'
 function App() {
 
   const options = [
@@ -22,12 +23,12 @@ function App() {
 
   ]
 
+  const [hasError, setHasError] = useState(false)
   const [timeh, setTimeh] = useState('loading...')
   const [nowd, setNowd] = useState('')
   const [latlon, setLatlon] = useState()
   const [weather, setWeather] = useState()
   const [temperature, setTemperature] = useState()
-  
   const [location, setLocation] = useState('')
 
   useEffect(() => {
@@ -42,7 +43,12 @@ function App() {
     }
     
     const error = err => {
-      console.log(err);
+      //console.log(err);
+      setHasError(true)
+
+/*       setTimeout(() => {
+        setHasError(false)
+      }, 5000) */
     }
     navigator.geolocation.getCurrentPosition(success, error);
   }, [])
@@ -53,21 +59,26 @@ function App() {
 
       const apikey = 'e7a1a2cfa98becafeefff6f39e6543e8'
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latlon.lat}&lon=${latlon.lon}&appid=${apikey}`
-      //const url2 = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apikey}`
 
       axios.get(url)
       .then(res => {
 
+        //console.log(res.data);
+
         const celsius = (res.data.main.temp - 273.15).toFixed(1)
         const farenheit = (celsius * 9/5 + 32).toFixed(1)
-        //console.log(celsius.toFixed(1) + '--'+farenheit.toFixed(1));
+
         const obj = {
           celsius, farenheit
         }
         setTemperature(obj)   
-        //setTemperature({celsius, farenheit})   
         setWeather(res.data)
-        })
+
+        //Oculto el select de los paises:
+        document.getElementById('divLocation').style.display = 'none'
+        document.getElementById('currentLocation').style.display = 'inline'
+
+      })
       .catch(err => console.log(err))
     }
 
@@ -78,6 +89,9 @@ function App() {
     const today = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    //console.log(Intl.DateTimeFormat().resolvedOptions());
+
     options.timeZone = timezone//'UTC';
     //options.timeZoneName = 'short';
     //const now = today.toLocaleString('en-US', options);
@@ -116,7 +130,6 @@ function App() {
     if (element === 'inline') {
         document.getElementById('divLocation').style.display = 'none'
         document.getElementById('currentLocation').style.display = 'inline'
-        
     }else{
         document.getElementById('divLocation').style.display = 'inline'
         document.getElementById('currentLocation').style.display = 'none'
@@ -138,7 +151,6 @@ function App() {
       axios.get(url)
       .then(res => {
         //console.log(res.data[0].lat);
-
           const obj = {
             lat: res.data[0].lat,
             lon: res.data[0].lon,
@@ -156,15 +168,16 @@ function App() {
   <div>
 
     <VideoComponent weather={weather} />
-{/*     <video autoPlay muted loop id="myVideo">      
-      <source src={weather} type="video/mp4"></source>
-      Your browser does not support HTML5 video.
-    </video> */}
 
     <div className="content">
       <div className="weather">
-
         {
+          hasError 
+          ? <p className='errormsg'>
+              Se requieren permisos <FontAwesomeIcon icon={faLocationPinLock} beat /> <br/>
+              Active la ubicación de su dispositivo
+             </p>
+          : 
           weather
           ?
           <>
@@ -195,7 +208,8 @@ function App() {
             </section>           
             <WeatherCard weather={weather} temperature={temperature}/> 
           </>
-          : <Loading/>
+          : 
+          <Loading/>
         }
       </div>
     </div>  
